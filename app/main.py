120976bebab815c0574a0e7e272dadff76ed940e
androcatz1 <narraj2381@gmail.com>
2026-07-05 22:14:17 +0800

    included analytics router and its endpoints

--- app/main.py
+++ app/main.py
@@ -1,8 +1,9 @@
 from fastapi import FastAPI
-from app.routers import comments, videos
+from app.routers import comments, videos, analytics
 
 
 app = FastAPI()
 
 app.include_router(comments.router)
 app.include_router(videos.router)
+app.include_router(analytics.router)
