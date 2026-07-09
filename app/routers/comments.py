99d79c96949d7cf52aa63c284ea50960e463f651
androcatz1 <narraj2381@gmail.com>
2026-07-09 12:21:14 +0800

    added comment dashboard endpoint

--- app/routers/comments.py
+++ app/routers/comments.py
@@ -1,9 +1,7 @@
 from fastapi import Depends, APIRouter, Query
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from app.schemas.comments import FilterSchema
 from app.database.connection import get_db_static
-from app.services.comment_dashboard import get_card_data
 from app.services.comments import check_video_id_exist
 from app.services.pipeline.comments import new_comments_pipeline
 
@@ -17,8 +15,3 @@ async def get_new_comments(db: AsyncSession = Depends(get_db_static), video_id: 
     
     status = await new_comments_pipeline(db, video_id)
     return status;
-
-@router.get("/comment_dashboard")
-async def get_comment_values(db:AsyncSession = Depends(get_db_static), filters: FilterSchema = Depends()):
-    comment_data = await get_card_data(db, filters, 20)
-    return comment_data
